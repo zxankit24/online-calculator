4fe67b6a68c5f28a652754858b7e08e6fad19401
zxankit24 <zxankit24@gmail.com>
2023-10-02 16:02:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        const expression = document.getElementById('display').value;
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+function backspace() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
